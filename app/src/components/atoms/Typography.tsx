2d7eb9b08df72a59146198c6f57b2c15ed999caa
Ivan <freitasjr.ivan@gmail.com>
2024-02-01 13:51:51 -0300

    feat: add color prop based on theme into Typography

--- app/src/components/atoms/Typography.tsx
+++ app/src/components/atoms/Typography.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components'
+import { Theme } from '../../styles/Theme'
 
 type TypographyProps = {
+  children: React.ReactNode
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
   $variant?: 'heading' | 'subheading' | 'body' | 'caption' | 'caption2'
-  children: React.ReactNode
+  $color?: keyof Theme['color']['text']
 }
 
 export const Typography = styled.p<TypographyProps>`
-  ${({ theme, $variant = 'body' }) => `
+  ${({ theme, $variant = 'body', $color = 'base' }) => `
     font-size: ${theme.font.size[$variant]};
+    color: ${theme.color.text[$color]};
     font-family: ${theme.font.family};
     font-weight: ${{
       heading: theme.font.weight.bold,
