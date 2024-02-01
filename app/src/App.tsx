7f543e5aa4bb3a131f0430c52eb7e7b2cd7fede5
Ivan <freitasjr.ivan@gmail.com>
2024-02-01 01:00:31 -0300

    fix: styled component warnings

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,15 @@
+import { Input } from './components/atoms/Input'
 import { Typography } from './components/atoms/Typography'
 import { ThemeProvider } from './styles/Theme'
 
 export function App() {
   return (
     <ThemeProvider>
-      <Typography as="h1" variant='heading'>Hello, todo-list!</Typography>
-      <Typography variant='subheading'>Hello, todo-list!</Typography>
-      <Typography variant='body'>Hello, todo-list!</Typography>
-      <Typography variant='caption'>Hello, todo-list!</Typography>
-      <Typography variant='caption2'>Hello, todo-list!</Typography>
+      <Typography as="h1" $variant='heading'>Hello, todo-list!</Typography>
+      <Typography $variant='subheading'>Hello, todo-list!</Typography>
+      <Typography $variant='body'>Hello, todo-list!</Typography>
+      <Typography $variant='caption'>Hello, todo-list!</Typography>
+      <Typography $variant='caption2'>Hello, todo-list!</Typography>
     </ThemeProvider>
   )
 }
--- app/src/components/atoms/Typography.tsx
+++ app/src/components/atoms/Typography.tsx
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 type TypographyProps = {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span'
-  variant?: 'heading' | 'subheading' | 'body' | 'caption' | 'caption2'
+  $variant?: 'heading' | 'subheading' | 'body' | 'caption' | 'caption2'
   children: React.ReactNode
 }
 
 export const Typography = styled.p<TypographyProps>`
-  ${({ theme, variant = 'body' }) => `
-    font-size: ${theme.font.size[variant]};
+  ${({ theme, $variant = 'body' }) => `
+    font-size: ${theme.font.size[$variant]};
     font-family: ${theme.font.family};
     font-weight: ${{
       heading: theme.font.weight.bold,
@@ -16,7 +16,7 @@ export const Typography = styled.p<TypographyProps>`
       body: theme.font.weight.regular,
       caption: theme.font.weight.regular,
       caption2: theme.font.weight.light,
-    }[variant]};
-    text-transform: ${['caption2'].includes(variant) ? 'uppercase' : 'none'};
+    }[$variant]};
+    text-transform: ${['caption2'].includes($variant) ? 'uppercase' : 'none'};
   `}
 `
