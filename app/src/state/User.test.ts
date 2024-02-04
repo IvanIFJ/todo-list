7eb5ec6aef11c30616e4cb93a60bd22e6d8e9bd2
Ivan <freitasjr.ivan@gmail.com>
2024-02-04 11:16:48 -0300

    test: add tooling for tests

---
+++ app/src/state/User.test.ts
@@ -0,0 +1,22 @@
+import { act, renderHook } from "@testing-library/react-hooks"
+import { describe, expect, it } from 'vitest'
+import { useUser } from '.'
+
+describe("UserStore", () => {
+  it("should initialize with no user informed", () => {
+    const { result } = renderHook(() => useUser());
+    expect(result.current.name).toEqual('');
+  });
+
+  it("should be possible to change the user", () => {
+    const {result} = renderHook(() => useUser());
+
+    const userNameA = 'John Doe';
+    act(() => result.current.setUser(userNameA));
+    expect(result.current.name).toEqual(userNameA);
+
+    const userNameB = 'Marie Doe';
+    act(() => result.current.setUser(userNameB));
+    expect(result.current.name).toEqual(userNameB);
+  });
+});
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-list/'
+  base: '/todo-list/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './testSetup.ts',
+  }
 })
