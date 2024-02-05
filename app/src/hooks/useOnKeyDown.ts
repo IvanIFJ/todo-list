5237831cb34e5eca8a726b389b90ddb9352f8a2c
Ivan <freitasjr.ivan@gmail.com>
2024-02-05 12:07:02 -0300

    feat: add useonkeydown hook

---
+++ app/src/hooks/useOnKeyDown.ts
@@ -0,0 +1,8 @@
+import { useEffect } from 'react'
+
+export const useOnKeyDown = (callback: (event: KeyboardEvent) => void) => {
+  useEffect(() => {
+    document.addEventListener('keydown', callback)
+    return () => document.removeEventListener('keydown', callback)
+  }, [callback])
+}
